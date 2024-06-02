c40d3d5ea41ee7db63b4ad73f27801517f8b54f0
Roman Khabarov <roman@globalart.dev>
2024-06-02 22:28:56 +0200

    feat: replce placeholders in object with flatten

--- src/helpers/replace-placeholers/index.ts
+++ src/helpers/replace-placeholers/index.ts
@@ -22,6 +22,36 @@ export const replacePlaceholdersInObject = (obj: unknown, replacements: unknown)
 }
 
 /**
+ * Replace placeholder in object with flatter replacements
+ * @param value 
+ * @param replacements 
+ * @returns 
+ */
+export const replacePlaceholdersInStringWithFlatten = (value: string, replacements: unknown) => {
+  const flattenVariables = (obj: any, prefix = ''): Record<string, unknown> => {
+    return Object.keys(obj).reduce((acc, k) => {
+      const pre = prefix.length ? `${prefix}.` : '';
+      if (typeof obj[k] === 'object' && obj[k] !== null) {
+        Object.assign(acc, flattenVariables(obj[k] as Record<string, unknown>, `${pre}${k}`));
+      } else {
+        acc[`${pre}${k}`] = obj[k];
+      }
+      return acc;
+    }, {} as Record<string, unknown>);
+  };
+
+  if (replacements) {
+    const flatVariables = flattenVariables(replacements);
+    Object.keys(flatVariables).forEach((variable) => {
+      const regex = new RegExp(`{{${variable}}}`, 'g');
+      value = value.replace(regex, String(flatVariables[variable]));
+    });
+  }
+
+  return value;
+}
+
+/**
  * Replace placeholder in the string
  */
 export const replacePlaceholdersInString = (value: string, replacements: unknown) => {
