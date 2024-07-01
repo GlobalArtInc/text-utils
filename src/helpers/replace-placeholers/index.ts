df8049c35116f9497a83a5fa83ae9a9b0c881aab
Roman Khabarov <roman@globalart.dev>
2024-07-01 04:26:08 +0000

    fix

--- src/helpers/replace-placeholers/index.ts
+++ src/helpers/replace-placeholers/index.ts
@@ -2,42 +2,52 @@ type Replacements = { [key: string]: string | number };
 
 /**
  * Replace placeholder in object
- * @param obj 
- * @param replacements 
- * @returns 
+ * @param obj
+ * @param replacements
+ * @returns
  */
-export const replacePlaceholdersInObject = (obj: unknown, replacements: unknown) => {
+export const replacePlaceholdersInObject = (obj: unknown, replacements: Record<string, unknown>): unknown => {
   if (typeof obj === 'string') {
-    const val = obj.replace(/{{(.*?)}}/g, (_, key) => replacements[key] ?? _);
-    
-    return !isNaN(parseFloat(val)) ? Number(val) : val;      
+    const replacedValue = obj.replace(/{{(.*?)}}/g, (_, key) =>
+      replacements[key] !== undefined ? replacements[key].toString() : _,
+    );
+
+    return !isNaN(replacedValue as unknown as number) ? Math.floor(parseFloat(replacedValue)) : replacedValue;
   }
-  if (obj && typeof obj === 'object') {
+
+  if (obj && typeof obj === 'object' && !Array.isArray(obj)) {
+    const result: Record<string, unknown> = {};
     Object.entries(obj).forEach(([key, value]) => {
-      obj[key] = replacePlaceholdersInObject(value, replacements);
+      result[key] = replacePlaceholdersInObject(value, replacements);
     });
+
+    return result;
   }
 
   return obj;
-}
+};
 
 /**
  * Replace placeholder in object with flatter replacements
- * @param value 
- * @param replacements 
- * @returns 
+ * @param value
+ * @param replacements
+ * @returns
  */
 export const replacePlaceholdersInObjectWithFlatten = (value: string, replacements: unknown) => {
   const flattenVariables = (obj: any, prefix = ''): Record<string, unknown> => {
-    return Object.keys(obj).reduce((acc, k) => {
-      const pre = prefix.length ? `${prefix}.` : '';
-      if (typeof obj[k] === 'object' && obj[k] !== null) {
-        Object.assign(acc, flattenVariables(obj[k] as Record<string, unknown>, `${pre}${k}`));
-      } else {
-        acc[`${pre}${k}`] = obj[k];
-      }
-      return acc;
-    }, {} as Record<string, unknown>);
+    return Object.keys(obj).reduce(
+      (acc, k) => {
+        const pre = prefix.length ? `${prefix}.` : '';
+        if (typeof obj[k] === 'object' && obj[k] !== null) {
+          Object.assign(acc, flattenVariables(obj[k] as Record<string, unknown>, `${pre}${k}`));
+        } else {
+          acc[`${pre}${k}`] = obj[k];
+        }
+
+        return acc;
+      },
+      {} as Record<string, unknown>,
+    );
   };
 
   if (replacements) {
@@ -49,11 +59,11 @@ export const replacePlaceholdersInObjectWithFlatten = (value: string, replacemen
   }
 
   return value;
-}
+};
 
 /**
  * Replace placeholder in the string
  */
 export const replacePlaceholdersInString = (value: string, replacements: unknown) => {
   return value.replace(/{{(.*?)}}/g, (_, key) => replacements[key] ?? _);
-}
+};
